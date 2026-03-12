d037c7442cef3809e7e146e5e608babb1e533cee
ootr <git@ootr.jp>
2026-03-12 18:57:53 +0900

    不要なasyncを削除

--- src/routes/api/v1/config.ts
+++ src/routes/api/v1/config.ts
@@ -2,7 +2,7 @@ import type { Hono } from "hono";
 import { legalUrl } from "@/const/env";
 
 export const registerApiV1Config = (app: Hono) => {
-  app.get("/api/v1/config", async (c) => {
+  app.get("/api/v1/config", (c) => {
     return c.json({
       legalUrl: legalUrl ?? null,
     });
